7742c40025251936ba1aae7ad673d894f86857a2
adityarawat4 <rawatadii060@gmail.com>
2024-11-14 14:24:25 +0530

    c

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ComingSoon from './components/ComingSoon.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <ComingSoon />
   </StrictMode>,
 )
