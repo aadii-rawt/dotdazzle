1df906fa856d70f9ba83c21c6f49bbb9b59935c2
adityarawat4 <rawatadii060@gmail.com>
2024-11-20 21:50:51 +0530

    c

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,11 +1,12 @@
 import { Helmet } from "react-helmet-async";
+import FavIcon from '../../public/Favicon/favicon.png'
 
 const SEO = () => (
     <Helmet>
-    <title>DotDazzle - Your Idea, Our Code | Web Development & Design Solutions</title>
+    <title>dotdazzle - Your Idea, Our Code | Web Development & Design Solutions</title>
     <meta 
         name="description" 
-        content="DotDazzle is your trusted software agency offering top-notch web development, graphic design, SEO, logo creation, and branding services. Turn your ideas into reality with us!" 
+        content="dotdazzle is your trusted software agency offering top-notch web development, graphic design, SEO, logo creation, and branding services. Turn your ideas into reality with us!" 
     />
     <meta 
         name="keywords" 
@@ -13,7 +14,7 @@ const SEO = () => (
     />
     <meta name="robots" content="index, follow" />
     <link rel="canonical" href="https://dotdazzle.in" />
-    <link rel="shortcut icon" href="./Favicon/dotdazzle-logo.png" type="image/x-icon" />
+    <link rel="shortcut icon" href={FavIcon} type="image/x-icon" />
 </Helmet>
 
 );
