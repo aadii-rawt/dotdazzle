942b2dc0908343631201913e8b74cad88d395713
Sahillrathore <sahil921031@gmail.com>
2024-11-20 23:33:28 +0530

    ..cm

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -5,42 +5,23 @@ function OurTeam() {
         <div className='max-w-[1280px] mx-auto section'>
             <h1 className='text-4xl font-russo text-center mb-20'>Our Team</h1>
             <div className='flex flex-wrap items-center justify-evenly'>
-                <div className='rounded-2xl space-y-3 p-4'>
-                    <div className='w-full flex items-center justify-center rounded-2xl overflow-hidden'>
-                        <img src="./sher.jpg" alt="out team founder" className='rounded-2xl'/>
-                    </div>
-                    <div className='text-center space-y-2'>
-                        <h1 className='font-russo text-xl'>Sher</h1>
-                        <h2 className='font-josefin'>Founder & CEO </h2>
-                    </div>
-                </div>
-                <div className='rounded-2xl space-y-3 p-5'>
-                    <div className='w-full flex items-center justify-center rounded-2xl overflow-hidden'>
-                        <img src="./cheetah.jpg" alt="out team founder" className='rounded-2xl'/>
-                    </div>
-                    <div className='text-center space-y-2'>
-                        <h1 className='font-russo text-xl'>Cheetah</h1>
-                        <h2 className='font-josefin'>Co-Founder</h2>
-                    </div>
-                </div>
-                <div className='rounded-2xl space-y-3 p-5'>
-                    <div className='w-full flex items-center justify-center rounded-2xl overflow-hidden'>
-                        <img src="./cheetah.jpg" alt="out team founder" className='rounded-2xl'/>
-                    </div>
-                    <div className='text-center space-y-2'>
-                        <h1 className='font-russo text-xl'>Cheetah</h1>
-                        <h2 className='font-josefin'>Project Manager</h2>
-                    </div>
-                </div>
-                <div className='rounded-2xl space-y-3 p-5'>
-                    <div className='w-full flex items-center justify-center rounded-2xl overflow-hidden'>
-                        <img src="./cheetah.jpg" alt="out team founder" className='rounded-2xl'/>
-                    </div>
-                    <div className='text-center space-y-2'>
-                        <h1 className='font-russo text-xl'>Cheetah</h1>
-                        <h2 className='font-josefin'>Project Manager</h2>
-                    </div>
-                </div>
+                {
+                    [{ img: './sher.jpg', title: 'Sher', desg: 'Founder' },
+                    { img: './cheetah.jpg', title: 'Cheetah', desg: 'Co-Founder' },
+                    { img: './jaadu.png', title: 'Jaadu', desg: 'Project Manager' },
+                    { img: 'https://w0.peakpx.com/wallpaper/569/648/HD-wallpaper-ghim-ca-%C2%A7a-ervanhovanes-hovanes-traan-oggy-oggy-and-jack.jpg', title: 'Jack', desg: 'Founder' },]
+                        .map((item) => (
+                            <div className='rounded-2xl space-y-3 p-4'>
+                                <div className='w-full flex items-center justify-center rounded-2xl overflow-hidden'>
+                                    <img src={item.img} alt="out team founder" className='rounded-2xl w-[225px] h-[225px]' />
+                                </div>
+                                <div className='text-center space-y-2'>
+                                    <h1 className='font-russo text-xl'>{item.title}</h1>
+                                    <h2 className='font-josefin'>{item.desg}</h2>
+                                </div>
+                            </div>
+                        ))
+                }
             </div>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 
 const projects = ["./responsive-website-project1.png", "./responsive-website-project2.jpg", "./responsive-website-project3.jpg", "https://s3.envato.com/files/326861897/PREVIEW_SET_ZIP/PSD-1.jpg", "./responsive-website-project5.jpg", "./responsive-website-project6.png",]
 function Projects() {
+
+    const cardVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0 }
+    };
+
     return (
         <section title='project-section' className='max-w-[1280px] mx-auto section'>
             <h1 className='text-4xl font-russo text-center mb-20'>Our Projects</h1>
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 px-3 md:px-0'>
-                {projects.map((data) => (
-                    <motion.div key={data} initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.8, ease: 'easeOut' }}
+                {projects.map((data, index) => (
+                    <motion.div key={data}
+                        initial="hidden"
+                        whileInView="visible"
+                        variants={cardVariants}
+                        transition={{ duration: 0.8, delay: index * 0.2, ease: 'easeInOut' }}
+                        viewport={{ once: false, amount: 0.2 }}
                         className='rounded-2xl overflow-hidden shadow-md  hover:scale-110'>
                         <img src={data} alt="" />
                     </motion.div>
