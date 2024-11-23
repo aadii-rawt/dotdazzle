6ad62cef871b3ef26b14923649d495eb96184f9b
adityarawat4 <rawatadii060@gmail.com>
2024-11-23 21:52:04 +0530

    commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { motion, useAnimation } from 'framer-motion'
 import { HiColorSwatch } from 'react-icons/hi';
+import { CgAwards, CgComponents, CgSearchFound } from 'react-icons/cg';
+import { CiMobile3 } from 'react-icons/ci';
+import { FaLink } from 'react-icons/fa';
 
 const services = [
     {
@@ -67,22 +70,37 @@ const Services = () => {
             <div className='max-w-[1280px] mx-auto overflow-hidden'>
                 <h1 className='sm:text-4xl text-3xl font-russo text-center mb-20 relative z-30 '>Our Services</h1>
                 <div className="relative overflow-hidden flex flex-col items-center md:flex-row justify-center p-6 ">
+                    {/* <div
+                        className={`relative overflow-hidden w-80 md:w-72 lg:w-96  text-left rounded-xl shadow-lg transform transition-transform duration-300 ease-in-out hover:-translate-y-2 -mr-16 group  bg-[#A5C8D8]  -rotate-[4deg] translate-y-4 cursor-pointer`} >
+                        <div className="absolute h-[0rem] w-[0rem] -z-10 rounded-full bg-[#10242F]duration-500 -top-20  -left-20  group-hover:h-[600px] group-hover:w-[34rem] transition-all ease-in-out"></div> */}
                     <div
-                        className={`relative overflow-hidden w-80 md:w-72 lg:w-96 p-6 text-left rounded-xl shadow-lg transform transition-transform duration-300 ease-in-out hover:-translate-y-2 -mr-16 group bg-[#10242F] group-hover:bg-[#A5C8D8] -rotate-[4deg] translate-y-4 cursor-pointer`} >
-                        <div className="absolute h-[0rem] w-[0rem] -z-10 rounded-full bg-[#A5C8D8] duration-500 -top-20  -left-20  group-hover:h-[600px] group-hover:w-[34rem] transition-all ease-in-out"></div>
-                        <h3 className="text-3xl text-[#A5C8D8] group-hover:text-[#10242F] duration-500  font-semibold mb-2">App</h3>
-                        <p className=" text-[#A5C8D8] group-hover:text-[#10242F] duration-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem earum eius error placeat consequatur corrupti.</p>
-                        <div className='bg-[#A5C8D8] group-hover:bg-[#10242F] duration-500 p-3 rounded-lg mt-4'>
-                            <div className='flex justify-between items-center'>
-                                <div className='flex gap-1'>
-                                    <div className='w-3 h-3 bg-[#10242F] group-hover:bg-[#A5C8D8] duration-500 rounded-full'></div>
-                                    <div className='w-3 h-3 bg-[#10242F] group-hover:bg-[#A5C8D8] duration-500 rounded-full'></div>
-                                    <div className='w-3 h-3 bg-[#10242F] group-hover:bg-[#A5C8D8] duration-500 rounded-full'></div>
-                                </div>
-                                <p className='text-[#10242F] group-hover:text-[#A5C8D8] duration-500 text-sm font-medium'>Photography</p>
+                        className={`relative overflow-hidden w-80 md:w-72 lg:w-96  text-left rounded-xl shadow-lg transform transition-transform duration-300 ease-in-out hover:-translate-y-2 -mr-16 group  bg-[#A5C8D8]  -rotate-[4deg] translate-y-4 cursor-pointer`}
+                    >
+                        <div className="absolute h-[0rem] w-[0rem] -z-10 rounded-full bg-[#10242F] duration-500 -top-20  -left-20  group-hover:h-[600px] group-hover:w-[34rem] transition-all ease-in-out"></div>
+                        <div className='p-6'>
+                            <h3 className="text-3xl text-[#10242F]  group-hover:text-[#A5C8D8] duration-500  font-semibold mb-2">SEO</h3>
+                            <p className=" group-hover:text-[#A5C8D8]  text-[#10242F] duration-500">We help your website shine brighter in search results with thoughtful and effective SEO strategies tailored to your needs.</p>
+                        </div>
+                        <div className=' rounded-lg mt-4'>
+                            <div className='flex gap-3 items-center bg-[#10242F] py-3 px-6 text-[#A5C8D8] group-hover:text-[#10242F]  group-hover:bg-[#A5C8D8] font-medium duration-500'>
+                                <span><CgSearchFound size={20} /></span>
+                                <h1>Keyword Research</h1>
                             </div>
-                            <div className='my-3 w-full h-44 bg-[#10242F] group-hover:bg-[#A5C8D8] duration-500 rounded-md'>
-
+                            <div className='flex gap-3 items-center text-[#10242F] group-hover:text-[#A5C8D8] py-3 px-6 bg-[#A5C8D8] group-hover:bg-[#10242F] font-medium duration-500'>
+                                <span><CgComponents size={20} /></span>
+                                <h1>Technical SEO</h1>
+                            </div>
+                            <div className='flex gap-3 items-center bg-[#10242F] py-3 px-6 text-[#A5C8D8] group-hover:text-[#10242F]  group-hover:bg-[#A5C8D8] font-medium duration-500'>
+                                <span><CiMobile3 size={20} /></span>
+                                <h1>Mobile SEO</h1>
+                            </div>
+                            <div className='flex gap-3 items-center text-[#10242F] group-hover:text-[#A5C8D8] py-3 px-6 bg-[#A5C8D8] group-hover:bg-[#10242F] font-medium duration-500'>
+                                <span><CgAwards size={20} /></span>
+                                <h1>SEO Audits</h1>
+                            </div>
+                            <div className='flex gap-3 items-center bg-[#10242F] py-3 px-6 text-[#A5C8D8] group-hover:text-[#10242F]  group-hover:bg-[#A5C8D8] font-medium'>
+                                <span><FaLink size={20} /></span>
+                                <h1>Link Building</h1>
                             </div>
                         </div>
                     </div>
@@ -117,17 +135,17 @@ const Services = () => {
                         <div className="absolute h-[0rem] w-[0rem] -z-10 rounded-full bg-[#1F1738] duration-500 -top-20  -left-20  group-hover:h-[600px] group-hover:w-[35rem] transition-all ease-in-out"></div>
 
                         <h3 className="text-3xl text-[#1F1738] group-hover:text-[#CBB0EB] duration-500 font-semibold mb-2">Graphic Design</h3>
-                        <p className=" text-[#1F1738] group-hover:text-[#CBB0EB] duration-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem earum eius error placeat consequatur corrupti.</p>
+                        <p className=" text-[#1F1738] group-hover:text-[#CBB0EB] duration-500 fon"> Transforms ideas into stunning visuals with creative and impactful graphic design services tailored to your brand.</p>
                         <div className='flex items-center justify-center mt-14 w-[500px] gap-3 overflow-hidden '>
                             <div className='w-40 h-40 bg-[#1F1738] group-hover:bg-[#CBB0EB] duration-500 rounded-3xl flex items-center justify-center'>
-                               <h1 className='text-center text-[#CBB0EB] group-hover:text-[#1F1738] text-5xl font-semibold font-serif'>Aa</h1> 
+                                <h1 className='text-center text-[#CBB0EB] group-hover:text-[#1F1738] text-5xl font-semibold font-serif'>Aa</h1>
                             </div>
                             <div className='w-40 h-40 bg-[#1F1738] group-hover:bg-[#CBB0EB] duration-500 rounded-full flex items-center justify-center'>
-                            <HiColorSwatch className='text-[#CBB0EB] group-hover:text-[#1F1738] text-5xl' />
+                                <HiColorSwatch className='text-[#CBB0EB] group-hover:text-[#1F1738] text-5xl' />
                             </div>
                             <div className='w-40 h-40 bg-[#1F1738] group-hover:bg-[#CBB0EB] duration-500 rounded-[50px]'></div>
                         </div>
-                       
+
                     </div>
 
                 </div>
