1199c9323d0511bb6f0ff1885307cd1c614910f0
Sahillrathore <sahil921031@gmail.com>
2024-12-01 15:51:11 +0530

    cmt

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
+import { motion } from 'framer-motion'
 
 const GetQuoteModal = () => {
     const [isOpen, setIsOpen] = useState(false); // Modal open state
@@ -56,7 +57,11 @@ const GetQuoteModal = () => {
                     <div className="bg-gradient-to-br from-purple-50/20 to-purple-200/40 sm:min-w-[28rem] min-w-[85%] rounded-md shadow-purple-400 shadow-[5px_5px_0px_0px_rgba(147,_51,_234,_0.5)]">
 
 
-                        <div className="bg-white/40 rounded-lg shadow-lg w-full p-6 relative backdrop-blur-[21px] backdrop-saturate-[200%] border border-gray-300/30">
+                        <motion.div
+                            initial={{ opacity: 0, y: -50 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.8, ease: 'easeOut' }}
+                            className="bg-white/40 rounded-lg shadow-lg w-full p-6 relative backdrop-blur-[21px] backdrop-saturate-[200%] border border-gray-300/30">
 
                             {/* <div> */}
                                 <img src="comma.png" alt=".comma" className="w-12 absolute sm:-top-6 -top-4 -left-4 sm:-left-5 z-20" />
@@ -159,7 +164,7 @@ const GetQuoteModal = () => {
                                     </button>
                                 </div>
                             </form>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             )}
